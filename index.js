f5a66d906bf5e53773f14e5f97812a7a14f76927
ruler88 <helloworld0424@gmail.com>
2014-09-27 21:19:23 -0700

    yolo

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var url = require('url');
-var app = express();
+var GCM = require('gcm').GCM;
+var apiKey = 'AIzaSyBSbZfBTrAH4xXdnk_1iVLRclNTWiUcWmY';
+var gcm = new GCM(apiKey);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+
+var sendPush = function() {
+	var message = {
+		registration_id: 'APA91bGnxRsa5Fhq2-WuLu4I1Hk71liH28ceVOHA_zvCEcXXaJbBLw55A4_nwZ5HFNNmpJ6Krx-F4X3IjXLC8VVzJhypGAwEFqD2GtwxeNKbBElLd_AnhTcYs1ZQLDexg2Xfvn3TeT6Jkl1iqJb0f2YYu7t0IwzTzZId13jmnp5jJGnC0jsVT_w',
+		message: "this is a nice message",
+		title: 'title4life'
+	};
+	gcm.send(message, function(err, messageId){
+		if (err) {
+			console.log("Something has gone wrong!");
+		} else {
+			console.log("Sent with message ID: ", messageId);
+		}
+	});
+};
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!');
 	var queryData = url.parse(request.url, true).query;
+	sendPush();
 	console.log(queryData);
 });
 
