802b42f8c01e8c1bdd96be610e4ef594789de610
ruler88 <helloworld0424@gmail.com>
2014-09-27 20:57:01 -0700

    log request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var express = require('express')
+var express = require('express');
+var url = require('url');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -6,6 +7,8 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
+	var queryData = url.parse(request.url, true).query;
+	console.log(queryData);
 });
 
 app.listen(app.get('port'), function() {
