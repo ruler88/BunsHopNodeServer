2b8efb19f9d1cbfda09ec9df22dd3593c04f37e1
ruler88 <helloworld0424@gmail.com>
2014-09-27 23:00:54 -0700

    rm stuff

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var GCM = require('gcm').GCM;
 var apiKey = 'AIzaSyBSbZfBTrAH4xXdnk_1iVLRclNTWiUcWmY';
 var gcm = new GCM(apiKey);
 
+var registeredUsers = {
+	Kai: '',
+	Sarah: ''
+};
+
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -29,10 +34,7 @@ var sendPush = function() {
 app.get('/', function(request, response) {
   response.send('Hello World!');
 	var queryData = url.parse(request.url, true).query;
-	sendPush();
 	console.log(queryData);
-});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+
 });
