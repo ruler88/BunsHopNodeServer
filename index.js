5572245839de832d1a78b30f570c8ba36d695e6c
ruler88 <helloworld0424@gmail.com>
2014-09-27 23:46:48 -0700

    lol

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var sendPush = function(first_name) {
 	}
 
 	sender.send(message, recipients, 4, function(err, result) {
-		console.log(result);
+		console.log("result from send: \n" + result);
 	});
 };
 
